5de9189ff188be41659be41f607c3747abee5633
saudiwin <bobkubinec@gmail.com>
2018-01-25 10:32:06 -0500

    adding correct data

--- R/other_doc.R
+++ R/other_doc.R
@@ -19,3 +19,10 @@
 #' @format A rollcall vote object that is a list with \code{votes} as the legislator-bill rollcall vote matrix.
 #' @source \url{https://www.voteview.com}
 "senate114"
+
+#' Fitted \code{idealstan} model for 114th Senate Data
+#' 
+#' This R object is a fitted \code{idealstan} object that can be used for testing and illustratory purposes.
+#' 
+#' @format A fitted \code{idealstan} object
+"senate114_fit"
